21a3b33066c18abc322ec4c998f4f9761dd15f00
fbex <8228317+fbex@users.noreply.github.com>
2024-01-20 13:01:44 +0100

    Enable vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from 'next/font/google'
 import './globals.css'
 import Navbar from "@/app/ui/Navbar";
 import Footer from "@/app/ui/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
     </body>
     </html>
   )
